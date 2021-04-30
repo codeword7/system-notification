8b02375fcde9d898f4020491ca98c6eeb14fc994
Neeraj Yadav <ny29599@gmail.com>
2021-04-30 20:30:11 +0530

    add notification to alert

--- app/js/moniter.js
+++ app/js/moniter.js
@@ -4,7 +4,13 @@ const cpu = osu.cpu
 const mem = osu.mem
 const os = osu.os
 
-let cpuOverload = 80;
+let cpuOverload = 5;
+
+notifyUser({
+  title: 'CPU Overload',
+  body: `CPU is over ${cpuOverload}`,
+  icon: path.join(__dirname, 'img', 'icon.png')
+})
 
 //run every 2 seconds
 setInterval(() => {
@@ -54,4 +60,9 @@ function secondsToDhms(seconds) {
   const m = Math.floor((seconds % 3600) / 60)
   const s = Math.floor(seconds % 60)
   return `${d}d, ${h}h, ${m}m, ${s}s`
+}
+
+//send notification
+function notifyUser(options) {
+  new Notification(options.title, options)
 }
